ddfb134c04363bc60561552b36e66650e0a79729
Root <sam.pugh@dbpixelhouse.com>
2021-01-28 22:02:54 +0000

    Added the page wrapper, time to ether start the 3js work or maybe about me as these will be the hardest.. or I could do more structure, could also just make a branch for each, see how I'm feeling.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Home from "components/home"
 import ThreeD from "components/3d"
 import Unity from "components/unity"
 import Navagation from "components/navagation"
-import GlobalSyles from "components/globalStyles"
+import GlobalSyles, { PageWrapper } from "components/globalStyles"
 
 const App = () => <div className="app">
 	<GlobalSyles />
 	<Navagation />
-	<Switch>
-		<Route exact path="/" render={Home}/>
-		<Route exact path="/aboutme" render={Home}/>
-		<Route exact path="/3js" render={ThreeD} />
-		<Route exact path="/unity" render={Unity} />
-	</Switch>
+	<PageWrapper>
+		<Switch>
+			<Route exact path="/" render={Home}/>
+			<Route exact path="/aboutme" render={Home}/>
+			<Route exact path="/3js" render={ThreeD} />
+			<Route exact path="/unity" render={Unity} />
+		</Switch>
+	</PageWrapper>
 </div>
 
 export default App
--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -1,13 +1,20 @@
-import { createGlobalStyle } from "styled-components"
+import Styled, { createGlobalStyle } from "styled-components"
 
 export const breakPoint = {
     maxWidth: "1500px",
     minWidth: "600px"
 }
+
 const windowSizes = {
     width: "100%",
     height: "100%",
 }
+
+export const PageWrapper = Styled.div(() => ({
+    ...breakPoint,
+    width: "80%",
+    margin: "0 auto"
+}))
 
 const GlobalSyles = createGlobalStyle(() => (
     {
